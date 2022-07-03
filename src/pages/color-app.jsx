efaf655c512fd9e71d7ac9f1652cdf4b875e6889
ronafa <ronafa@post.bgu.ac.il>
2022-07-03 12:18:58 +0300

    add socket

--- src/pages/color-app.jsx
+++ src/pages/color-app.jsx
@@ -1,12 +1,18 @@
 import { useEffect, useState } from 'react'
 import { colorService } from '../services/color.service'
+import { socketService, SOCKET_EVENT_ADD_VOTE, SOCKET_EMIT_CLICK_VOTE } from '../services/socket.service'
+
 import { ColorList } from '../cmps/color-list'
 
 export function ColorApp() {
     const [colors, setColors] = useState(null)
 
     useEffect(() => {
+        socketService.on(SOCKET_EVENT_ADD_VOTE, addVote)
         loadColors()
+        return () => {
+            socketService.off(SOCKET_EVENT_ADD_VOTE, addVote)
+        }
     }, [])
 
     const loadColors = async () => {
@@ -26,6 +32,7 @@ export function ColorApp() {
     const onUpdateColor = async (color) => {
         try {
             const updatedColor = await colorService.update(color)
+            socketService.emit(SOCKET_EMIT_CLICK_VOTE, {...updatedColor})
             addVote(updatedColor)
         } catch (err) {
             console.log('Error in onUpdateColor', err)
--- src/services/color.service.js
+++ src/services/color.service.js
@@ -1,19 +1,14 @@
 import { httpService } from './http.service'
 
-// import { storageService } from './async-storage.service'
-// const STORAGE_KEY = 'color'
-
 export const colorService = {
     query,
     update
 }
 
 async function query() {
-    // return await storageService.query(STORAGE_KEY)
     return await httpService.get('color')
 }
 
 async function update(color) {
-    // return await storageService.put(STORAGE_KEY, color)
     return await httpService.put('color', color)
 }
